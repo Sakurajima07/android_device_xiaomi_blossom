f57d07c790af11c6c7cbe2f888703a448e6785e0
Sakurajima <sakuta.sakurajima07@gmail.com>
2025-07-26 18:43:19 +0400

    Revert "blossom: AndroidProducts.mk"

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_blossom.mk
+    $(LOCAL_DIR)/lineage_blossom.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_blossom-user \
-    aosp_blossom-userdebug \
-    aosp_blossom-eng
+    lineage_blossom-user \
+    lineage_blossom-userdebug \
+    lineage_blossom-eng
