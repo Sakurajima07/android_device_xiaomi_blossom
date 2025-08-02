dd6e1b9b80b5f949840ea9a7a05bd868776fa8b0
Sakurajima <sakuta.sakurajima07@gmail.com>
2025-08-02 17:48:08 +0400

    blossom: create infinity_blossom.mk

---
+++ infinity_blossom.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, device/xiaomi/blossom/device.mk)
+
+# Inherit some common infinity stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+scr_resolution := 720
+TARGET_SUPPORTS_BLUR := true
+TARGET_ENABLE_BLUR := true
+TARGET_BOOT_ANIMATION_RES := 720
+
+PRODUCT_NAME := infinity_blossom
+PRODUCT_DEVICE := blossom
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Redmi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+INFINITY_BUILD_TYPE := UNOFFICIAL
+INFINITY_MAINTAINER := Sakurajima
